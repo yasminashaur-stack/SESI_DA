99760794584bae611eb5e0d3bbf566db39f90992
yasmin <yasmin.ashaur@aluno.senai.br>
2026-09-14 08:54:45 -0300

    Aula_07correcao

--- Aula_07correcao/script.js
+++ Aula_07correcao/script.js
@@ -1,4 +1,16 @@
 
-function jogar(){
-    
+var ordem_jogada = true;
+
+function jogar(num_btn){
+    const botao = document.getElementById("num_btn");
+
+if (ordem_jogada === true){
+    botao.innerHTML = "o";
+   // ordem_jogada = false;
+}else{
+    botao.innerHTML = "x";
+   // ordem_jogada = true;
+    }
+
+    ordem_jogada = !ordem_jogada;
 }

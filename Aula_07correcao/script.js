8077176ccdb0b27c4f9fe3dd77c90a6c1d967464
yasmin <yasmin.ashaur@aluno.senai.br>
2026-09-14 10:13:00 -0300

    Aula_07correcao

--- Aula_07correcao/script.js
+++ Aula_07correcao/script.js
@@ -9,8 +9,71 @@ if (ordem_jogada === true){
    // ordem_jogada = false;
 }else{
     botao.innerHTML = "x";
-   // ordem_jogada = true;
+
     }
 
+    ValidarGanhador();
     ordem_jogada = !ordem_jogada;
+}
+function validarGanhador() {
+
+    const btn1_vlr = document.getElementById("1").innerHTML;
+    const btn2_vlr = document.getElementById("2").innerHTML;
+    const btn3_vlr = document.getElementById("3").innerHTML;
+    const btn4_vlr = document.getElementById("4").innerHTML;
+    const btn5_vlr = document.getElementById("5").innerHTML;
+    const btn6_vlr = document.getElementById("6").innerHTML;
+    const btn7_vlr = document.getElementById("7").innerHTML;
+    const btn8_vlr = document.getElementById("8").innerHTML;
+    const btn9_vlr = document.getElementById("9").innerHTML;
+
+    if ((btn1_vlr == btn2_vlr)
+        && (btn1_vlr == btn3_vlr)
+        && (btn1_vlr !== "")
+        && (btn2_vlr !== "")
+        && (btn3_vlr !== "")) {
+        alert("O ganhador foi " + ordem_jogada ? "X" : "O");
+    } else if ((btn4_vlr == btn5_vlr)
+        && (btn4_vlr == btn6_vlr)
+        && (btn4_vlr !== "")
+        && (btn5_vlr !== "")
+        && (btn6_vlr !== "")) {
+        alert("O ganhador foi " + ordem_jogada ? "X" : "O");
+    } else if ((btn7_vlr == btn8_vlr)
+        && (btn7_vlr == btn9_vlr)
+        && (btn7_vlr !== "")
+        && (btn8_vlr !== "")
+        && (btn9_vlr !== "")) {
+        alert("O ganhador foi " + ordem_jogada ? "X" : "O");
+    } else if ((btn1_vlr == btn4_vlr)
+        && (btn4_vlr == btn7_vlr)
+        && (btn1_vlr !== "")
+        && (btn4_vlr !== "")
+        && (btn7_vlr !== "")) {
+        alert("O ganhador foi " + ordem_jogada ? "X" : "O");
+    } else if ((btn2_vlr == btn5_vlr)
+        && (btn2_vlr == btn8_vlr)
+        && (btn2_vlr !== "")
+        && (btn5_vlr !== "")
+        && (btn8_vlr !== "")) {
+        alert("O ganhador foi " + ordem_jogada ? "X" : "O");
+    } else if ((btn3_vlr == btn6_vlr)
+        && (btn3_vlr == btn9_vlr)
+        && (btn3_vlr !== "")
+        && (btn6_vlr !== "")
+        && (btn9_vlr !== "")) {
+        alert("O ganhador foi " + ordem_jogada ? "X" : "O");
+    } else if ((btn1_vlr == btn5_vlr)
+        && (btn1_vlr == btn9_vlr)
+        && (btn1_vlr !== "")
+        && (btn5_vlr !== "")
+        && (btn9_vlr !== "")) {
+        alert("O ganhador foi " + ordem_jogada ? "X" : "O");
+    } else if ((btn3_vlr == btn5_vlr)
+        && (btn3_vlr == btn7_vlr)
+        && (btn3_vlr !== "")
+        && (btn5_vlr !== "")
+        && (btn7_vlr !== "")) {
+        alert("O ganhador foi " + ordem_jogada ? "X" : "O");
+    }
 }
